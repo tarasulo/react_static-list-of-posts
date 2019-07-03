2b06fac5a5cb58de62b0744fbcca0e4a8b881bc3
Taras Khalak <tarasulo@gmail.com>
2019-07-03 20:10:07 +0300

    Updates

---
+++ precache-manifest.1960b55ece8506cbe0721f4867978765.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bdb7a1d6813081eaeb3439011697cef",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "b6494285d0dcbd38058b",
+    "url": "/react_static-list-of-posts/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "0d93b29b27d1d199691f",
+    "url": "/react_static-list-of-posts/static/js/2.95678282.chunk.js"
+  },
+  {
+    "revision": "b6494285d0dcbd38058b",
+    "url": "/react_static-list-of-posts/static/js/main.d188959c.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
